cb877e8391841acce31faaecd2cb40ce4ea55660
isirajieinnocent <isirajieinnocent@gmail.com>
2025-07-30 00:37:34 +0300

    updated

--- github-oidc/backend.tf
+++ github-oidc/backend.tf
@@ -1,3 +1,8 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket         = "backend-bucket-yourname"  # Must match exactly
+    key            = "github-oidc/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
 }
---
+++ github-oidc/terraform.tfvars
@@ -0,0 +1,4 @@
+environment      = "dev"
+github_org       = "Inno"
+github_repo      = "github-aws-oidc-integration"
+project_name     = "aws-oidc-integration"
